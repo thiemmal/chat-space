6c3dbe1f1561efaaf490b6740a7ab8c2b23a5459
thiemmal <takuto_0914@icloud.com>
2020-07-04 17:40:38 +0900

    create group-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,12 +11,24 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-
+  end
     
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(group_params[:id])
+    if @group.save
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:create, :new]
+  resources :groups, only: [:create, :new, :edit, :update]
 end
