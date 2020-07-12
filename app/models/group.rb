af71671f121ff8291c2bf2056cc94586cf92c73f
thiemmal <takuto_0914@icloud.com>
2020-07-12 13:32:44 +0900

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,18 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    last_message = messages.last
+    if last_message.present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージがありません。'
+    end
+  end
+
 end
