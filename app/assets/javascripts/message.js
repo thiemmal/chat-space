7aa9aba698e863e8d294eb5ad33a9a9f5876bb64
thiemmal <takuto_0914@icloud.com>
2020-08-09 21:41:38 +0900

    update-message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,43 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html =`<div class="main-chat__message-list__forth-items">
+                  <div class="main-chat__message-list__forth-items__upper">
+                    <div class="main-chat__message-list__forth-items__upper__name">
+                      ${message.user_name}
+                    </div>
+                    <p class="main-chat__message-list__forth-items__upper__time">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <div class="main-chat__message-list__forth-items__lower">
+                    <p class="main-chat__message-list__forth-items__lower__message">
+                      ${message.body}
+                    </p>
+                    <img class="main-chat__message-list__forth-items__lower__image" src="${message.image}">
+                  </div>
+                </div>`
+                return html;
+    } else {
+      let html = 
+    `<div class="main-chat__message-list__forth-items">
+      <div class="main-chat__message-list__forth-items__upper">
+        <div class="main-chat__message-list__forth-items__upper__name">
+          ${message.user_name}
+        </div>
+        <p class="main-chat__message-list__forth-items__upper__time">
+          ${message.created_at}
+        </p>
+      </div>
+      <div class="main-chat__message-list__forth-items__lower">
+        <p class="main-chat__message-list__forth-items__lower__message">
+          ${message.body}
+        </p>
+      </div>
+    </div>`
+    return html;
+    };
+  }
   $('#new_form').on('submit',function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -11,5 +50,10 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
+    .done(function(data){
+      let html = buildHTML(data)
+      $('.main-chat__message-list').append(html);
+      $('#new_form')[0].reset();
+    })
+  });
 });
