d2d8fc2231c6151cdd9cd9dc88f5e539abb93cfa
thiemmal <takuto_0914@icloud.com>
2020-08-10 19:29:09 +0900

    update

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,20 @@
 $(function(){
+  function appendUser(user) {
+    let html = `<div class="ChatMember clearfix">
+                  <p class="ChatMember__name">${user.name}</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    $('#UserSearchResult').append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
 
   $("#UserSearch__field").on("keyup", function(){
     let input = $("#UserSearch__field").val();
@@ -6,7 +22,25 @@ $(function(){
       type: 'GET',    
       url: '/users',       
       dataType: 'json',
-      data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+      data: { keyword: input },
+    })
+    .done(function(users){
+      $(".UserSearchResult").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      }
+      else if (input.length == 0) {
+        return false;
+      } 
+      else {
+        addNoUser();
+      }
+      
+    })
+    .fail(function(){
+      alert("通信エラーです。ユーザーが表示できません。");
     })
   })
 });
